a3a03003afa88df4824d63fdefe6cb86e2ed1202
hanna.k <hanna.k0616@gmail.com>
2022-03-26 21:40:43 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Muahhh!!!!!!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://de.linkedin.com/in/simon-riess"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          where does this lead to....?
         </a>
       </header>
     </div>
